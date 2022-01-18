f5fdb3c06a5454cc55b30104fc0a1d989a540a2a
Nick Granados <internick2017@gmail.com>
2022-01-18 17:24:24 -0300

    implementar proptypes y actualizar snapshots

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import { shallow } from "enzyme";
 import { GifGridItem } from "../../components/GifGridItem";
 
 describe('pruebas en gif grid item', () => {
+    const title = 'un titulo';
+    const url = 'https://localhost/algo.jpg';
+    
     test('deve mostrar componente correctamente ', () => {
-        const wrapper = shallow(<GifGridItem />)
+        const wrapper = shallow(<GifGridItem title={title} url={url} />)
         expect(wrapper).toMatchSnapshot();
     })
 
